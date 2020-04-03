cd88b8fbe8497f627c87c4787ed17bc96670c232
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-04-03 22:39:43 +0800

    init project

---
+++ src/main/java/cn/lmsite/lmsnav/LmsNavApplication.java
@@ -0,0 +1,13 @@
+package cn.lmsite.lmsnav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsNavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsNavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/lmsite/lmsnav/LmsNavApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lmsite.lmsnav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsNavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
