9453e3dab571b1cde868ef8cc769c5b8e173018f
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-04-06 23:46:47 +0800

    更新于:2020-04-06 23:46:43

--- deploy-lmsnav.sh
+++ deploy-lmsnav.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-echo "删除【前】项目根目录内容为："
-echo `ls -la /webroot/nav.lmsite.cn/`
-rm -rf /webroot/nav.lmsite.cn/*
-echo "删除【后】项目根目录内容为："
-echo `ls -la /webroot/nav.lmsite.cn/`
+echo "开始停止应用："
+pid=`ps -ef | grep lms-nav.jar | grep -v grep | awk '{print $2}'`
+if [ -n "$pid" ]
+then
+#!kill -9 强制终止
+   echo "kill -9 的pid:" $pid
+   kill -9 $pid
+fi
+# 备份
+file_path=/home/jonny/.jenkins/workspace/lmsnav-ci/target
+mv /webroot/nav.lmsite.cn/lms-nav.jar /backups/lmsnav_bak/lmsnav.jar.`date +%Y%m%d%H%M%S`
+
+echo `ls -A /webroot/nav.lmsite.cn/`
+
 echo "复制新的 jar 包..."
-cp $WORKSPACE/target/lms-nav-0.9.0.jar /webroot/nav.lmsite.cn/lms-nav.jar
-java -jar /webroot/nav.lmsite.cn/lms-nav.jar
+cp /home/jonny/.jenkins/workspace/lmsnav-ci/target/lms-nav-0.9.0.jar /webroot/nav.lmsite.cn/lms-nav.jar
+
+ehco "授予当前用户权限"
+chmod 777 /webroot/nav.lmsite.cn/lms-nav.jar
+echo "执行....."
+cd /webroot/nav.lmsite.cn/
+java -jar lms-nav.jar
 echo "成功启动项目："
 echo `ps -ef | grep lms-nav.jar`
 
