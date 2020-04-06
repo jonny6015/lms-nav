be70a5992faa0527a99596c777f04ed25641e758
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-04-06 18:21:42 +0800

    更新于:2020-04-06 18:21:37

--- deploy-lmsnav.sh
+++ deploy-lmsnav.sh
@@ -5,7 +5,7 @@ rm -rf /webroot/nav.lmsite.cn/*
 echo "删除【后】项目根目录内容为："
 echo `ls -la /webroot/nav.lmsite.cn/`
 echo "复制新的 jar 包..."
-cp $WORKSPACE/$project_name/target/lms-nav-0.9.0.jar /webroot/nav.lmsite.cn/lms-nav.jar
+cp $WORKSPACE/target/lms-nav-0.9.0.jar /webroot/nav.lmsite.cn/lms-nav.jar
 java -jar /webroot/nav.lmsite.cn/lms-nav.jar
 echo "成功启动项目："
 echo `ps -ef | grep lms-nav.jar`
