8e4a6441edceb4bf7eddc742451bfbeb5ac7866e
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-04-06 18:10:55 +0800

    更新于:2020-04-06 18:10:51

--- deploy-lmsnav.sh
+++ deploy-lmsnav.sh
@@ -1,26 +1,13 @@
 #!/bin/sh
-echo "停止 Tomcat ..."
-echo "停止 Tomcat ..."
-echo `systemctl status tomcat.service`
-
-echo "删除旧的项目 ROOT 目录..."
-rm -rf /webroot/nav.lmsite.cn/ROOT
-echo "根目录内容为："
+echo "删除【前】项目根目录内容为："
 echo `ls -la /webroot/nav.lmsite.cn/`
-echo "删除旧的 war 包..."
-rm -rf /webroot/wars/lms-nav.jar
-echo "/webroot/wars 目录下的内容为："
-echo `ls -la /webroot/wars/`
-
-echo "复制新的 war 包..."
-cp $WORKSPACE/$project_name/target/lms-nav-0.9.0.jar /webroot/wars/lms-nav.jar
-echo "/webroot/wars 目录下的内容为："
-echo `ls -la /webroot/wars/`
-
-echo "启动 Tomcat..."
-echo `systemctl start tomcat.service`
-echo `systemctl status tomcat.service`
-echo "Tomcat 启动成功！"
-
+rm -rf /webroot/nav.lmsite.cn/*
+echo "删除【后】项目根目录内容为："
+echo `ls -la /webroot/nav.lmsite.cn/`
+echo "复制新的 jar 包..."
+cp $WORKSPACE/$project_name/target/lms-nav-0.9.0.jar /webroot/nav.lmsite.cn/lms-nav.jar
+java -jar /webroot/nav.lmsite.cn/lms-nav.jar
+echo "成功启动项目："
+echo `ps -ef | grep lms-nav.jar`
 
 
