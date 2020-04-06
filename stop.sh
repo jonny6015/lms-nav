0480b6741a5a38ee8380f6b6326907f1dec6bede
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-04-06 16:58:42 +0800

    更新于:2020-04-06 16:58:38

---
+++ stop.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd /home/jenkins/HelloWorld
+echo "停止 lms-nav  应用...."
+pid=`ps -ef | grep lms-nav-0.9.0.jar | grep -v grep | awk '{print $2}'`
+# shellcheck disable=SC1035
+if ["$pid" == ""]
+    then
+        echo "lms-nav 已经停止了!"
+else
+    echo "lms-nav 的 pid 是:"+$pid
+    kill -9 $pid
+    echo "lms-nav 停止成功！"
+fi
